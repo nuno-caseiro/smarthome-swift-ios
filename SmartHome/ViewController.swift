d4dc66cdf52b7270f12e23ffd3e1c50bacbf5179
Nuno Caseiro <nunocaseiro@MBP-de-Nuno.Home>
2020-11-19 01:30:36 +0000

    Initial Commit

---
+++ SmartHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartHome
+//
+//  Created by Nuno Caseiro on 19/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
